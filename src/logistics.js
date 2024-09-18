43d2525aa463d501735ac566b52497d1be876400
ab226rf <ab226rf@student.lnu.se>
2024-09-18 13:14:39 +0200

    started statistics file and sum

---
+++ src/logistics.js
@@ -0,0 +1,39 @@
+class Logistics {
+
+    /**
+     * 
+     * @param {Array} numArray - 
+     * @returns 
+     */
+  sum(numArray) {
+    let result
+    for(let i = 0; i < numArray; i++) {
+      result += numArray[i]
+    }
+    return result
+  }
+
+  mean(numArray) {
+
+  }
+
+  median(numArray) {
+
+  }
+
+  sortByDescending(numArray) {
+
+  }
+
+  sortByAscending(numArray) {
+
+  }
+
+  convertToDecimal(numArray) {
+
+  }
+
+  convertToPercentage(numArray) {
+
+  }
+}
