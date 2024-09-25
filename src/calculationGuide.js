62a9306c1847502725134b4ad13629a6b0377710
ab226rf <ab226rf@student.lnu.se>
2024-09-25 19:13:33 +0200

    My first guide

--- src/calculationGuide.js
+++ src/calculationGuide.js
@@ -1,8 +1,19 @@
 import { Statistics } from './Statistics.js'
 
 class CalculationGuide extends Statistics {
+  /**
+   * Gives the user a mean guide.
+   *
+   * @returns {object} A object with the definition and steps to calculate mean.
+   */
   meanGuide() {
-
+    return {
+      definition: 'The mean is the average number in a set of numbers.',
+      steps: [
+        'Add all numbers together.',
+        'Divide the sum with the amount of numbers in the set.'
+      ]
+    }
   }
 
   medianGuide() {
@@ -22,6 +33,6 @@ class CalculationGuide extends Statistics {
   }
 
   rangeGuide() {
-    
+
   }
 }
