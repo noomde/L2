bf6fca02bea7c5eca0c59d33400d8e8127d4a6fc
ab226rf <ab226rf@student.lnu.se>
2024-09-25 19:24:50 +0200

    median guide done

--- src/calculationGuide.js
+++ src/calculationGuide.js
@@ -4,7 +4,7 @@ class CalculationGuide extends Statistics {
   /**
    * Gives the user a mean guide.
    *
-   * @returns {object} A object with the definition and steps to calculate mean.
+   * @returns {object} - A object with the definition and steps to calculate mean.
    */
   meanGuide() {
     return {
@@ -16,8 +16,19 @@ class CalculationGuide extends Statistics {
     }
   }
 
+  /**
+   *  Gives the user a median guide.
+   *
+   * @returns {object} - A object with the definition and steps to calculate median.
+   */
   medianGuide() {
-
+    return {
+      definition: 'The middle number in an ordered set of numbers. If the set is an even amount, the median is the average of the 2 middle numbers.',
+      steps: [
+        'Sort the numbers from smallest to biggest or biggest to smallest.',
+        'The median number is the number in the middle. If it is an even amount then the median is the average of the 2 middle numbers.'
+      ]
+    }
   }
 
   varianceGuide() {
